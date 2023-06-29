baf246c5756fa84ca05a29a5203a8904708269b5
Juhana Kuparinen <juhku@elisanet.fi>
2023-06-29 10:32:49 +0300

    cargo fmt

--- src/domain/new_subscriber.rs
+++ src/domain/new_subscriber.rs
@@ -3,6 +3,6 @@ use crate::domain::subscriber_name::SubscriberName;
 use crate::domain::SubscriberEmail;
 
 pub struct NewSubscriber {
-  pub email: SubscriberEmail,
-  pub name: SubscriberName,
+    pub email: SubscriberEmail,
+    pub name: SubscriberName,
 }
